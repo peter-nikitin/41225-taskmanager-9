acdcfd3ab4683f193cb9e5b8a78772d601b65144
Petr Nikitin <nikitin.p.94@gmail.com>
2019-08-26 22:58:16 +0300

    refactore main

--- src/main.js
+++ src/main.js
@@ -5,10 +5,14 @@ import {filtersLayout} from './components/filters.js';
 import {cardLayout} from './components/card.js';
 import {addCardLayout} from './components/addCard.js';
 import {loadMoreBtnLayout} from './components/loadMoreBtn.js';
+import {getTask as task} from './data.js';
+import {getFilter as filter} from './data.js';
 
-const renderElement = (element, layout) => {
-  element.innerHTML += layout();
+const renderElement = (element, layout, data) => {
+  element.innerHTML += layout(data);
 };
+
+const TASK_COUNT = 3;
 
 const menu = document.querySelector(`.main__control`);
 const main = document.querySelector(`.main`);
@@ -27,10 +31,14 @@ board.innerHTML += ` <div class="board__filter-list">
 <a href="#" class="board__filter">SORT BY DATE down</a>
 </div>`;
 
+const tasks = new Array(TASK_COUNT).fill(``).map(() => task());
+
 renderElement(boardTasks, addCardLayout);
-renderElement(boardTasks, cardLayout);
-renderElement(boardTasks, cardLayout);
-renderElement(boardTasks, cardLayout);
+tasks.map((currentTask) => {
+  renderElement(boardTasks, cardLayout, currentTask);
+});
+
+
 board.appendChild(boardTasks);
 renderElement(board, loadMoreBtnLayout);
 main.appendChild(board);
