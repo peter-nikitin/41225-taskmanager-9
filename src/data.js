cdb533700545cb76108e778491aefb1a0410bd4e
Никитин Петр <nikitin@MacBook-Pro-Admin.local>
2019-08-26 21:13:49 +0300

    add getFilter func

---
+++ src/data.js
@@ -0,0 +1,9 @@
+export const getFilter = (tasks) => ({
+  All: tasks.length,
+  Overdue: tasks.filter((task) => task.dueDate < Date.now()).length,
+  Today: tasks.filter((task) => task.dueDate === Date.now()).length,
+  Favorites: tasks.filter((task) => task.isFavorite).length,
+  Archive: tasks.filter((task) => task.isArchive).length,
+  Repeating: tasks.filter((task) => Object.keys(task.repeatingDays).some((day) => task.repeatingDays[day])).length,
+  Tags: tasks.length,
+});
