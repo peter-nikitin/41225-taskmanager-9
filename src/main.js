1dc10052c64551a18f70a6ea29aea3ded2501726
Никитин Петр <nikitin@MacBook-Pro-Admin-3.local>
2019-09-16 21:59:19 +0300

    made simpler

--- src/main.js
+++ src/main.js
@@ -1,23 +1,12 @@
-import {menuLayout} from './components/menu.js';
-import {searchLayout} from './components/search.js';
-import {filtersLayout} from './components/filters.js';
-import {getFilter, getTask} from './data.js';
+import {getTask} from './data.js';
 import BoardController from './controllers/boardController';
 
-const renderElement = (element, layout, data) => {
-  element.innerHTML += layout(data);
-};
 
 const TASK_COUNT = 3;
 
 const taskMock = Array(TASK_COUNT).fill(``).map(() => getTask());
 
-
-const menu = document.querySelector(`.main__control`);
 const main = document.querySelector(`.main`);
-renderElement(menu, menuLayout);
-renderElement(main, searchLayout);
-renderElement(main, filtersLayout, getFilter(taskMock));
 
 const boardController = new BoardController(main, taskMock);
 
